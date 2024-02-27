d100718d3c53a35361d4c3db1de0b5a5d4420080
Julian Harris <julian.harris@gmail.com>
2024-02-27 10:20:40 +0000

    basic pipeline experiments

--- pipelines.py
+++ pipelines.py
@@ -1,47 +1,78 @@
+import torch
+print(torch.__version__)
+
 import argparse
 import pandas as pd
 import time
 from transformers import pipeline
 from tqdm import tqdm
 import psutil
-import torch
+
+MODEL_PATH = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Sentiment Analysis with Transformers')
 parser.add_argument('--source', type=str, default='messages-binary-classifier.csv', help='Source CSV file containing messages')
 parser.add_argument('--score-threshold', type=float, default=0.85, help='Score threshold for classification')
+parser.add_argument('--limit', type=int, default=None, help='Limit the number of predictions to process')
+parser.add_argument('--model', type=str, default=MODEL_PATH, help='model to do sentiment analysis with')
+
 args = parser.parse_args()
 
 # Initialize the sentiment analysis classifier
-classifier = pipeline("sentiment-analysis")
+classifier = pipeline("sentiment-analysis", model=args.model, tokenizer=args.model)
+test_result = classifier("Hey idiot, can't you figure out this simple problem on your own?")
+print(f"Model test: {test_result}")
+if args.limit is not None:
+    print(f"Limiting to {args.limit} predictions")
+
+
 
 # Initialize confusion matrix and timing list
 confusion_matrix = {'TP': 0, 'FP': 0, 'TN': 0, 'FN': 0}
 inference_times = []
 
+# map the model result to the expected label from the data source
+result_mappings = {
+    'positive': False,
+    'neutral': False,
+    'negative': True
+}
+
 # Read the CSV file
 df = pd.read_csv(args.source)
+processed_predictions = 0
 
 # Iterate over the messages in the CSV file with a progress bar
 for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="Analyzing messages"):
+    processed_predictions += 1
+    if args.limit is not None and processed_predictions >= args.limit:
+        break
+
     message = row['message']
-    expected = row['is_respectful']  # Assuming the column is named 'expected_result'
+    actual_sentiment = row['is_disrespectful']  
     
     start_time = time.time()
     result = classifier(message)[0]
     inference_times.append(time.time() - start_time)
     
-    # Convert 'True' to 'NEGATIVE' and 'False' to 'POSITIVE' for comparison
-    expected_label = 'NEGATIVE' if expected else 'POSITIVE'
+    model_result = result_mappings.get(result['label'].lower(), None)
+    # Ensure the model result is valid (exists in our mapping)
+    if model_result is None:
+        print(f"Unexpected model result: {result['label']}, skipping")
+        continue  # or handle unexpected result labels as needed
+    
+    # Determine if the prediction matches the expected label
+    correct_prediction = (model_result == actual_sentiment)
     
     # Update confusion matrix based on the prediction and actual result
-    if result['label'] == expected_label and result['score'] > args.score_threshold:
-        if expected_label == 'NEGATIVE':
+    if correct_prediction and result['score'] > args.score_threshold:
+        if actual_sentiment:
             confusion_matrix['TP'] += 1
         else:
             confusion_matrix['TN'] += 1
     else:
-        if expected_label == 'NEGATIVE':
+        if actual_sentiment:
             confusion_matrix['FN'] += 1
         else:
             confusion_matrix['FP'] += 1
